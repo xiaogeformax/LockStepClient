22afe4fd01af4512d4a03e535cddad9c1a0a2eb0
xiaogeformax <554378242@qq.com>
2018-11-18 14:13:20 +0800

    初始化模型场景

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,61 @@
+﻿using Game;
+using net;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Util;
+
+public class Main : MonoBehaviour
+{
+
+    public string m_ip = "127.0.0.1";
+    public int m_tcpPort = 1255;
+    public int m_udpPort = 1337;
+
+    public Button btn_connect;
+    public Button btn_ready;
+    public Button btn_skill;
+    public int m_skillId = 1001;
+
+    AsycUdpClient m_client;
+
+    void Awake()
+    {
+        InvokeRepeating("Tick", 1, 0.02f);
+    }
+
+    void Tick()
+    {
+        TimerHeap.Tick();
+        FrameTimerHeap.Tick();
+    }
+
+    void Start()
+    {
+        SceneManager.instance.InitGame(m_ip, m_tcpPort, m_udpPort);
+        btn_connect.onClick.RemoveAllListeners();
+        btn_connect.onClick.AddListener(SceneManager.instance.viewMap.LogicMap.netManager.Connect);
+
+        btn_ready.onClick.RemoveAllListeners();
+        btn_ready.onClick.AddListener(SceneManager.instance.viewMap.LogicMap.netManager.Ready);
+
+        btn_skill.onClick.RemoveAllListeners();
+        btn_skill.onClick.AddListener(ClientDoSkill);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneManager.instance.Update();
+    }
+
+    public void ClientDoSkill()
+    {
+        SceneManager.instance.viewMap.LogicMap.InputCmd(Cmd.UseSkill, m_skillId.ToString());
+    }
+    void OnDisable()
+    {
+        SceneManager.instance.viewMap.LogicMap.netManager.Disconnect();
+    }
+}
--- Assets/Scripts/net/NetManager.cs
+++ Assets/Scripts/net/NetManager.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using net;
-
+using Net;
+using Util;
 
 namespace Game
 {
@@ -46,8 +47,36 @@ namespace Game
         {
             m_client.Update();
         }
-        //Todo 同步位置和关键帧
+      
+        /// <summary>
+        /// 同步自己的位置
+        /// </summary>
+        public void SycMePos()
+        {
+            Debug.Log("同步问题   ---------------------------");
+            Vector3 pos = SceneManager.instance.viewMap.CurViewObj.Pos;
+            Vector3 angle = SceneManager.instance.viewMap.CurViewObj.EulerAngles;
+            MessageBuffer msgBuf = new MessageBuffer();
+            msgBuf.WriteInt(cProto.SYNC_POS);
+            msgBuf.WriteInt(SceneManager.instance.viewMap.CurViewObj.Id);
+            string cPos = string.Format("{0}#{1}#{2}#{3}#{4}#{5}", pos.x, pos.y, pos.z, angle.x, angle.y, angle.z);
+            msgBuf.WriteString(cPos);
+            Send(msgBuf);
+        }
 
+        /// <summary>
+        /// 发送关键帧
+        /// </summary>
+        public void SycKey()
+        {
+            string keyStr = string.Join(";", m_keyPack.ToArray());
+            MessageBuffer msgBuf = new MessageBuffer();
+            msgBuf.WriteInt(cProto.SYNC_KEY);
+            msgBuf.WriteInt(SceneManager.instance.viewMap.LogicMap.m_curFrameCount);
+            msgBuf.WriteString(keyStr);
+            Send(msgBuf);
+            ClearKeyPack();
+        }
 
         /// <summary>
         /// 客户端准备
@@ -55,9 +84,30 @@ namespace Game
         public void Ready()
         {
             MessageBuffer msgBuf = new MessageBuffer();
-           /* msgBuf.WriteInt(cProto.READY);
-            msgBuf.WriteInt(SceneManager.instance.viewMap.LogicMap.curRoleId);*/
+            msgBuf.WriteInt(cProto.READY);
+            msgBuf.WriteInt(SceneManager.instance.viewMap.LogicMap.m_curRoleId);
             Send(msgBuf);
+        }
+
+        /// <summary>
+        /// 连接
+        /// </summary>
+        public void Connect()
+        {
+            Debug.Log("Connecting....");
+            m_client.Connect(_ip, _tcpPort, _udpPort);
+        }
+
+        /// <summary>
+        /// 断开连接
+        /// </summary>
+        public void Disconnect()
+        {
+            if (m_client.Connected)
+            {
+                m_client.Disconnect();
+                Debug.Log("Disconnect.....");
+            }
         }
 
         /// <summary>
@@ -132,8 +182,48 @@ namespace Game
             Debug.Log(cproto);
             switch (cproto)
             {
-
+                case cProto.CONNECT:
+                    //                   SceneManager.instance.InitViewMap();
+                    int roleId = msg.ReadInt();
+                    //                    SceneManager.instance.viewMap.CreateMe(roleId);
+                    SceneManager.instance.viewMap.LogicMap.m_curRoleId = roleId;
+                    Debug.Log("玩家,id = " + roleId);
+                    break;
+                case cProto.READY:
+                    break;
+                case cProto.SYNC_POS:
+                    int cRoleId = msg.ReadInt();
+                    string pos = msg.ReadString();
+                    SceneManager.instance.viewMap.SyncPos(cRoleId, pos);
+                    Debug.Log(string.Format("玩家 {0} ,pos = {1}", cRoleId, pos));
+                    break;
+                case cProto.SYNC_KEY:
+                    int servFrameCount = msg.ReadInt();
+                    if (servFrameCount >= SceneManager.instance.viewMap.LogicMap.m_curFrameCount)
+                    {
+                        string keyStr = msg.ReadString();
+                        string[] keyData = keyStr.Split(';');
+                        for (int i = 0; i < keyData.Length; ++i)
+                        {
+                            if (keyData[i] == "")
+                            {
+                                continue;
+                            }
+                            KeyData data = new KeyData(keyData[i]);
+                            SceneManager.instance.viewMap.LogicMap.DoCmd(data);
+                        }
+                        SceneManager.instance.viewMap.LogicMap.m_curFrameCount += 1;
+                    }
+                    break;
+                case cProto.START:
+                    string players = msg.ReadString();
+                    SceneManager.instance.viewMap.CreateAllPlayer(players);
+                    TimerHeap.AddTimer(0, 50, SycMePos);
+                    TimerHeap.AddTimer(0, 100, SycKey);
+                    break;
             }
+
+        
         }
     }
 
